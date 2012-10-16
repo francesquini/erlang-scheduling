a0f7a8b0392201e4a9ead7f66b01d5e71dd4cc68
francesquini <emilio.francesquini@imag.fr>
2012-10-16 11:25:25 +0200

    Scripts now check for "root" to be able to probe the VM, this avoids some nasty runtime errors.
    Now traces are generated in the correct directory

--- src/benchmarks/suites/ip_rounds.erl
+++ src/benchmarks/suites/ip_rounds.erl
@@ -20,18 +20,26 @@ run() ->
 	Times = 20,
 	Size = small,
 	scheduling:bind_no_spread(),
-	run_ip_default(Size, Times),
-	run_ip_circular(Size, Times),
-	run_ip_random(Size, Times).
+	run_ip (default, Size, Times),
+	run_ip (circular, Size, Times),
+	run_ip (random, Size, Times).
 
 generate_trace_script([OutFileName, Size, RoundMax, RoundStep]) ->
-	io:format("#!/bin/bash\n"),
-	io:format("DIR=$(dirname $0)\n"),
-	io:format("SIZE=~s\n", [Size]),
-	io:format("REPTS=1\n"),
-	RoundList = lists:seq(utils:to_int(RoundStep), utils:to_int(RoundMax), utils:to_int(RoundStep)),	
-	[io:format("$DIR/../erl_prof  $DIR/~s.~p.~p.trace ip_rounds run ~p ~p $SIZE $REPTS >$DIR/~s.~p.~p.res\n" ++
-			   "$DIR/../prof2paje $DIR/~s.~p.~p.trace $DIR/~s.~p.~p.paje\n", 
+	io:format(
+	  	"#!/bin/bash\n" ++
+		"DIR=$(dirname $0)\n" ++
+		"TRACE_DIR=$DIR/../../traces/\n" ++
+		"SIZE=~s\n" ++
+		"REPTS=1\n" ++
+		"if [ \"$(whoami)\" != \"root\" ]; then\n" ++
+		"	echo \"Sorry, to probe the Erlang VM you must run this script as root.\"\n" ++
+		"	exit 1\n" ++
+		"fi\n",
+		[Size]),  
+	  
+	RoundList = lists:seq(0, utils:to_int(RoundMax), utils:to_int(RoundStep)),	
+	[io:format("$DIR/../erl_prof  $TRACE_DIR/~s.~p.~p.trace ip_rounds run ~p ~p $SIZE $REPTS >$TRACE_DIR/~s.~p.~p.res\n" ++
+			   "$DIR/../prof2paje $TRACE_DIR/~s.~p.~p.trace $TRACE_DIR/~s.~p.~p.paje\n", 
 			   [OutFileName, Str, Rnds, Str, Rnds, OutFileName, Str, Rnds,
 				OutFileName, Str, Rnds, OutFileName, Str, Rnds]) || 
 		Str <- sched_ip_strategies:get_strategies(), Str /= default, 
@@ -41,38 +49,34 @@ generate_trace_script([OutFileName, Size, RoundMax, RoundStep]) ->
 %% Local Functions
 %%
 
-
-run_ip_default(Size, Times) ->	
-	scheduling:set_all_strategies_default(),
-	bench:do ("Default", Size, Times).
-
-run_ip_circular(Size, Times) ->	
-	scheduling:set_all_strategies_default(),
-	sched_ip_strategies:set_circular(),
-	bench:do ("Circular", Size, Times),
-	Waits = lists:seq(50, 1000, 50),
-	[run_in_strategy (circular, After, Size, Times) || After <- Waits].
-
-run_ip_random(Size, Times) ->	
-	scheduling:set_all_strategies_default(),
-	sched_ip_strategies:set_random(),
-	bench:do ("Random", Size, Times),
-	Waits = lists:seq(50, 1000, 50),
-	[run_in_strategy (random, After, Size, Times) || After <- Waits].
+run_ip (Strategy, Size, Times) ->
+	Waits = lists:seq(0, 100, 5),
+	[run_in_strategy (Strategy, After, Size, Times) || After <- Waits].
 
 
+run_in_strategy (Strategy, 0, Size, Times) when is_atom(Strategy) andalso is_atom(Size) andalso is_integer(Times) ->
+	InitFun = fun() -> 
+				  scheduling:set_all_strategies_default(),
+				  sched_ip_strategies:set_strategy(Strategy)
+			  end,
+	run_in_strategy (Strategy, 0, Size, Times, InitFun);
 run_in_strategy (Strategy, After, Size, Times) when is_atom(Strategy) andalso is_integer(After) andalso is_atom(Size) andalso is_integer(Times) ->
+	InitFun = fun() ->
+				  scheduling:set_all_strategies_default(),
+				  sched_ip_strategies:set_strategy(Strategy),
+				  sched_ip_strategies:set_strategy_after(default, After)
+		  	  end,
+	run_in_strategy (Strategy, After, Size, Times, InitFun). 
+
+run_in_strategy (Strategy, After, Size, Times, InitFun) when is_function (InitFun) ->
 	Ref = make_ref(),
 	Self = self(),
 	spawn_link (
 	  fun() -> 
 		bench:do(
-		  utils:to_string(Strategy) ++ "-" ++ utils:to_string(After), Size, Times, 
-		  fun() -> 
-				  scheduling:set_all_strategies_default(),
-				  sched_ip_strategies:set_strategy(Strategy),
-				  sched_ip_strategies:set_strategy_after(default, After)
-		  end),
-		Self ! Ref	
+		  utils:to_string(Strategy) ++ "-" ++ utils:to_string(After), 
+		  	Size, Times, InitFun),
+		Self ! Ref
 	  end),
-	receive Ref -> ok end. 
+	receive Ref -> ok end.
+

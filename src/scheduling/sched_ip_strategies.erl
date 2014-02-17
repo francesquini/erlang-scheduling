6339f2ca20827d15ce514c15d5b67025ce2fb0e3
Emilio Francesquini <francesquini@gmail.com>
2014-02-17 13:32:28 -0300

    Adding support for multiple strategies for hubs and regular actors

--- src/scheduling/sched_ip_strategies.erl
+++ src/scheduling/sched_ip_strategies.erl
@@ -4,9 +4,13 @@
 %% Exported Functions
 %%
 -export([
-		 get_current_strategy/0,
-		 set_strategy/1,
-		 set_strategy_after/2,
+		 get_current_regular_strategy/0,
+		 get_current_hub_strategy/0,
+
+		 set_regular_strategy/1,
+		 set_hub_strategy/1,
+
+		 set_strategy_after/3,
 		 cancel_scheduled_strategy_change/0,
 		 set_default/0, 
 		 set_random/0,
@@ -14,35 +18,60 @@
 		 get_strategies/0
 		]).
 
-%%
-%% API Functions
-%%
+regular_key() ->
+	scheduler_ip_strategy_regular.
+hub_key() ->
+	scheduler_ip_strategy_hub.
 
-get_current_strategy() ->
-	StId = erlang:system_info(scheduler_ip_strategy),
+get_current_strategy(Key) ->
+	StId = erlang:system_info(Key),
 	strategy_name(StId).
 
-set_default() ->
-	set_strategy(default).
-set_random() ->
-    set_strategy(random).
-set_circular() ->
-	set_strategy(circular).
+%% API
+get_current_regular_strategy() ->
+	get_current_strategy(regular_key()).
 
-set_strategy (Strategy) ->
+%% API
+get_current_hub_strategy() ->
+	get_current_strategy(hub_key).
+
+%% API
+set_regular_strategy(Strategy) ->
+	set_strategy (Strategy, regular_key()).
+
+%% API	
+set_hub_strategy(Strategy) ->
+	set_strategy (Strategy, hub_key()).
+
+set_default() ->
+	set_regular_strategy(default),
+	set_hub_strategy(default).
+set_random() ->
+    set_regular_strategy(random),
+    set_hub_strategy(random).
+set_circular() ->
+	set_regular_strategy(circular),
+	set_hub_strategy(random).
+
+set_strategy (Strategy, Key) ->
 	scheduling:check_scheduler_bindings(),
 	StId = strategy_id(Strategy),
 	OldValue = erlang:system_flag(scheduler_ip_strategy, StId),
 	strategy_name(OldValue).
 
-set_strategy_after (Strategy, After_CBS) when is_integer(After_CBS) andalso After_CBS > 0 ->
+set_strategy_after (Strategy, After_CBS, hub) ->
+	set_strategy_after_for (Strategy, After_CBS, hub_key());
+set_strategy_after (Strategy, After_CBS, regular) ->
+	set_strategy_after_for (Strategy, After_CBS, regular_key()).
+
+set_strategy_after_for (Strategy, After_CBS, Key) when is_integer(After_CBS) andalso After_CBS > 0 ->
 	scheduling:check_scheduler_bindings(),
 	StId = strategy_id(Strategy),
-	OldValue = erlang:system_flag(scheduler_ip_strategy, {StId, 'after', After_CBS}),
+	OldValue = erlang:system_flag(Key, {StId, 'after', After_CBS}),
 	strategy_name(OldValue).
 	
 cancel_scheduled_strategy_change () ->
-	strategy_name(erlang:system_flag(scheduler_ip_strategy, {0, 'after', 0})).
+	strategy_name(erlang:system_flag(regular_key(), {0, 'after', 0})). %Any key will do
 
 get_strategies() ->
 	[Str || {Str, _} <- strategies()].
